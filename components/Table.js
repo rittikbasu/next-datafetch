6c7736b864c86911943bd79498f09e6a8e989b6b
rittikbasu <irittik@gmail.com>
2022-11-18 00:15:39 +0530

    feat: add search functionality

--- components/Table.js
+++ components/Table.js
@@ -4,10 +4,37 @@ export default function Table(data) {
   // paginate data
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(10);
-  const itemCount = useState(data.data.length);
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = data.data.slice(
+  const [query, setQuery] = useState("");
+
+  const handleChange = (e) => {
+    setQuery(e.target.value.toLowerCase());
+    setUserdata(searchData(e.target.value.toLowerCase()));
+  };
+
+  const searchData = (query) => {
+    if (!query) {
+      return data.data;
+    }
+    const searchCategory =
+      document.getElementById("search-category").value;
+
+    let filteredData = data.data
+      .filter((user) =>
+        user[searchCategory].toLowerCase().startsWith(query)
+      )
+      .map((user) => {
+        return user;
+      });
+
+    setCurrentPage(1);
+    return filteredData;
+  };
+
+  const [userdata, setUserdata] = useState(searchData());
+  const itemCount = userdata.length;
+  const currentItems = userdata.slice(
     indexOfFirstItem,
     indexOfLastItem
   );
@@ -27,80 +54,77 @@ export default function Table(data) {
       setCurrentPage(currentPage - 1);
     }
   };
-
   return (
     <div className="container mx-auto px-4 sm:px-8">
-      <div className="py-8">
-        <div>
-          <h2 className="text-2xl font-semibold leading-tight">
-            Users
-          </h2>
+      <div>
+        <div className="py-8 text-center">
+          <h1 className="text-5xl text-[#f34d76] font-semibold leading-tight">
+            <span className="text-blue-700">Super</span>mind
+          </h1>
         </div>
-        <div className="my-2 flex sm:flex-row flex-col">
+        <div className="my-2 flex flex-row justify-center">
           <div className="flex flex-row mb-1 sm:mb-0">
             <div className="relative">
-              <select className="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                <option>5</option>
-                <option>10</option>
-                <option>20</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
-            </div>
-            <div className="relative">
-              <select className="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
-                <option>All</option>
-                <option>Active</option>
-                <option>Inactive</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
-            </div>
-          </div>
-          <div className="block relative">
-            <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
-              <svg
-                viewBox="0 0 24 24"
-                className="h-4 w-4 fill-current text-gray-500"
+              <select
+                className="appearance-none h-full rounded-l border block appearance-none w-full bg-red-200 border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-gray-500"
+                id="search-category"
               >
-                <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"></path>
-              </svg>
-            </span>
-            <input
-              placeholder="Search"
-              className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
-            />
+                <option value="first_name">Name</option>
+                <option value="email">Email</option>
+                <option value="username">Username</option>
+                <option value="gender">Gender</option>
+                <option>Status</option>
+                <option>City</option>
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg
+                  className="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </div>
+            <div className="block relative">
+              <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                <svg
+                  viewBox="0 0 24 24"
+                  className="h-4 w-4 fill-current text-gray-500"
+                >
+                  <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"></path>
+                </svg>
+              </span>
+              <input
+                placeholder="Search"
+                className="appearance-none rounded-r sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
+                onChange={handleChange}
+              />
+            </div>
           </div>
         </div>
+
         <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
           <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
             <table className="min-w-full leading-normal">
               <thead>
                 <tr>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-blue-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                     Name
                   </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-blue-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                     Email
                   </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-blue-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                     Username
                   </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-blue-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Gender
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-blue-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    City
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-blue-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                     Status
                   </th>
                 </tr>
@@ -110,7 +134,7 @@ export default function Table(data) {
                 {/* {currentItems.map((item) => ( */}
                 {currentItems.map((user) => (
                   <tr key={user.id}>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <td className="px-5 py-2 border-b border-gray-200 bg-white text-sm">
                       <div className="flex items-center">
                         <div className="flex-shrink-0 w-10 h-10">
                           <img
@@ -126,17 +150,27 @@ export default function Table(data) {
                         </div>
                       </div>
                     </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <td className="px-5 py-2 border-b border-gray-200 bg-white text-sm">
                       <p className="text-gray-900 whitespace-no-wrap">
                         {user.email}
                       </p>
                     </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <td className="px-5 py-2 border-b border-gray-200 bg-white text-sm">
                       <p className="text-gray-900 whitespace-no-wrap">
                         {user.username}
                       </p>
                     </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <td className="px-5 py-2 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">
+                        {user.gender}
+                      </p>
+                    </td>
+                    <td className="px-5 py-2 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">
+                        {user.address.city}
+                      </p>
+                    </td>
+                    <td className="px-5 py-2 border-b border-gray-200 bg-white text-sm">
                       <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
                         <span
                           aria-hidden
@@ -151,26 +185,27 @@ export default function Table(data) {
                 ))}
               </tbody>
             </table>
-            <div className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
-              <span className="text-xs xs:text-sm text-gray-900">
-                Showing {indexOfFirstItem + 1} to {indexOfLastItem} of{" "}
-                {itemCount} Entries
-              </span>
-              <div className="inline-flex mt-2 xs:mt-0">
-                <button
-                  onClick={prevPage}
-                  className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l"
-                >
-                  Prev
-                </button>
-                <button
-                  onClick={nextPage}
-                  className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r"
-                >
-                  Next
-                </button>
-              </div>
-            </div>
+          </div>
+        </div>
+        <div className="px-5 py-5 bg-white flex flex-col xs:flex-row items-center xs:justify-between          ">
+          <span className="text-xs xs:text-sm text-gray-900">
+            Showing {indexOfFirstItem + 1} to{" "}
+            {itemCount % 10 === 0 ? indexOfLastItem : itemCount} of{" "}
+            {itemCount} Entries
+          </span>
+          <div className="inline-flex mt-2 xs:mt-0">
+            <button
+              onClick={prevPage}
+              className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l"
+            >
+              Prev
+            </button>
+            <button
+              onClick={nextPage}
+              className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r"
+            >
+              Next
+            </button>
           </div>
         </div>
       </div>
