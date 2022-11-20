56ed71eca096ba942cd56686ad5562e61a2150f6
rittikbasu <irittik@gmail.com>
2022-11-20 17:04:59 +0530

    feat: remove prev &  next btns for itemCount < 10

--- components/Table.js
+++ components/Table.js
@@ -21,20 +21,22 @@ export function Navigation({
               : indexOfLastItem}{" "}
             of {itemCount} Entries
           </span>
-          <div className="inline-flex mt-2 xs:mt-0">
-            <button
-              onClick={prevPage}
-              className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l"
-            >
-              Prev
-            </button>
-            <button
-              onClick={nextPage}
-              className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r"
-            >
-              Next
-            </button>
-          </div>
+          {itemCount > 10 && (
+            <div className="inline-flex mt-2 xs:mt-0">
+              <button
+                onClick={prevPage}
+                className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l"
+              >
+                Prev
+              </button>
+              <button
+                onClick={nextPage}
+                className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r"
+              >
+                Next
+              </button>
+            </div>
+          )}
         </div>
       ) : (
         <div className="text-center text-2xl text-gray-500">
