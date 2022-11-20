c57051f015d325d0a1bebd8c0fe03c4e9430d23b
rittikbasu <irittik@gmail.com>
2022-11-20 05:31:59 +0530

    fix: add key prop to TableHead

--- components/Table.js
+++ components/Table.js
@@ -50,7 +50,10 @@ export function TableHead({ fields }) {
     <thead>
       <tr>
         {fields.map((field, index) => (
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-blue-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+          <th
+            key={field}
+            className="px-5 py-3 border-b-2 border-gray-200 bg-blue-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider"
+          >
             {field}
           </th>
         ))}
